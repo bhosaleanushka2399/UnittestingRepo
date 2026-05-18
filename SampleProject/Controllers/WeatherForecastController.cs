6ab09a391c49e1fd8d2eef09bd87c5c8e0885fad
bhosaleanushka2399 <anushkabhosale1818@gmail.com>
2026-05-18 08:26:04 +0530

    Update WeatherForecastController.cs

--- SampleProject/Controllers/WeatherForecastController.cs
+++ SampleProject/Controllers/WeatherForecastController.cs
@@ -22,5 +22,19 @@ namespace SampleProject.Controllers
             })
             .ToArray();
         }
+
+        
+        [HttpGet(Name = "GetWeatherForecast1")]
+        public IEnumerable<WeatherForecast1> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        }
     }
 }
